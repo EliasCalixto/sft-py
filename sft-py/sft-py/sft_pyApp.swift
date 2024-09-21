6b79e02b8ba3a6c02570690b22c07588f535196f
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2024-09-20 19:55:49 -0500

    srg

---
+++ sft-py/sft-py/sft_pyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sft_pyApp.swift
+//  sft-py
+//
+//  Created by Elias Calixto on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct sft_pyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sft-py/sft-pyTests/sft_pyTests.swift
@@ -0,0 +1,16 @@
+//
+//  sft_pyTests.swift
+//  sft-pyTests
+//
+//  Created by Elias Calixto on 9/20/24.
+//
+
+import Testing
+
+struct sft_pyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
